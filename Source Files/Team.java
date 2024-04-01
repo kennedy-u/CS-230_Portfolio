42e73c113cbe32bce26c3504780d69b0ae224089
Kennedy Uzoho <91644837+kennedy-u@users.noreply.github.com>
2024-03-31 21:36:27 -0400

    Update Team.java

--- Source Files/Team.java
+++ Source Files/Team.java
@@ -6,14 +6,8 @@ import java.util.List;
 
 /**
 * A simple class to hold information about a team
-* <p>
-* Notice the overloaded constructor that requires
-* an id and name to be passed when creating.
-* Also note that no mutators (setters) defined so
-* these values cannot be changed once a team is
-* created.
-* </p>
-* @author coce@snhu.edu
+* 
+* @author kennedy.uzoho@snhu.edu
 *
 */
 public class Team extends Entity{
