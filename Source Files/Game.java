f79a8568cd4620d1929003428abee189c30bc95b
Kennedy Uzoho <91644837+kennedy-u@users.noreply.github.com>
2024-03-31 21:32:02 -0400

    Update Game.java

--- Source Files/Game.java
+++ Source Files/Game.java
@@ -7,15 +7,7 @@ import java.util.List;
 /**
 * A simple class to hold information about a game
 *
-* <p>
-* Notice the overloaded constructor that requires
-* an id and name to be passed when creating.
-* Also note that no mutators (setters) defined so
-* these values cannot be changed once a game is
-* created.
-* </p>
-*
-* @author coce@snhu.edu
+* @author kennedy.uzoho@snhu.edu
 *
 */
 public class Game extends Entity {
