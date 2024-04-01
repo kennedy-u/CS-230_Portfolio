335502b22d29ccafbe7f695fe9fed5866d28eec1
Kennedy Uzoho <91644837+kennedy-u@users.noreply.github.com>
2024-03-31 21:34:04 -0400

    Update Player.java

--- Source Files/Player.java
+++ Source Files/Player.java
@@ -2,14 +2,8 @@ package com.gamingroom;
 
 /**
 * A simple class to hold information about a player
-* <p>
-* Notice the overloaded constructor that requires
-* an id and name to be passed when creating.
-* Also note that no mutators (setters) defined so
-* these values cannot be changed once a player is
-* created.
-* </p>
-* @author coce@snhu.edu
+*
+* @author kennedy.uzoho@snhu.edu
 *
 */
 public class Player extends Entity{
